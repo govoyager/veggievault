a0ec3e0a311832b24431d83492179c6d530c4239
govoyager <govoyager2021@gmail.com>
2024-07-25 17:55:26 +0800

    Built web
    Branch: main
    Commit: df7d0cf1ec8540cce7d0d46619f5f7b9462db419
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -120,7 +120,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6a75ee316cd0f5baf76f6e796af903ee
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "8ed953f5b36db4760c11899abea2fb59",
+"flutter_bootstrap.js": "36b1e9ae2eaf53f64e6709291e39ac5e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
